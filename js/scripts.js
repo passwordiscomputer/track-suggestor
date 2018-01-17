ac71c671e27fd9001a46a7a65559b1b65112c368
Chase "Danger" Bowden <chase.bowden.pdx@gmail.com>
2018-01-16 21:48:39 -0800

    add logic branching and display/hide for answer and form

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,35 @@
 $(document).ready(function(){
-
+//Click form submit
 	$('#trackForm').submit(function(event){
+		//assign survey answers to variable names
 	    var frontOrBack = $("#fEvBE").val();
 	    var compSize = $("#size").val();
 	    var droid =$("#android").val();
 	    var wind = $("#windows").val();
 	    var des =$("#design").val();
-	    alert(frontOrBack);
 	    event.preventDefault();
+	   	if(frontOrBack === "Front End"){
+	   		if (des === "Yes") {
+	   			$(".css").show();
+	   		}else{
+	   			$(".js").show();
+	   		}
+	 	}else if(size === "Small Companies and Start-ups"){
+	 		$(".ruby").show();
+	   	}else if(droid === "Yes"){
+	   		$(".java").show();
+	   	}else if(wind === "Yes"){
+	   		$(".c").show();
+	   	}else{
+	   		$(".ruby").show();
+	   	}
+	   	$('.reset').show();
+	   	$("#trackForm").hide();
 	});
+	$('.reset').click(function(){
+		$('.reset').hide();
+	   	$("#trackForm").show();
+	   	$(".choices > div").hide();
+	})
 
 });
